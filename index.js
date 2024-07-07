b67e88bbad902817c3479937f5e908f882e98527
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2024-07-07 21:23:33 +0600

    :tada: Initialized Project

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const http = require("http");
+const app = require("./src/app");
+const connectDB = require("./src/db/connectDB");
+const server = http.createServer(app);
+const port = process.env.PORT;
+
+const main = async () => {
+  await connectDB();
+  server.listen(port, () => {
+    console.log(
+      `Server is running on port ${port}, Following link http://localhost:${port}`
+    );
+  });
+};
+main();
